01c53f6525d8a1dfefd964abfa3102b797a85d29
Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
2019-05-23 22:06:59 +0900

    add creds string getter cli

---
+++ get_creds_base64.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import pickle
+import base64
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    creds = flow.run_local_server()
+    creds_pickle = pickle.dumps(creds)
+    creds_b64 = base64.b64encode(creds_pickle)
+
+    print ("--> success\n\n" + creds_b64.decode('ascii') + "\n")
+
+if __name__ == '__main__':
+    main()
+
