c97186ee76f3a1e33539de579921cd73f9a98367
Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
2019-05-27 22:25:13 +0900

    add lib.py

--- get_creds_base64.py
+++ get_creds_base64.py
@@ -1,8 +1,5 @@
 from __future__ import print_function
-import pickle
-import base64
-
-from googleapiclient.discovery import build
+import lib
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
@@ -10,11 +7,10 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 def main():
     flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
     creds = flow.run_local_server()
-    creds_pickle = pickle.dumps(creds)
-    creds_b64 = base64.b64encode(creds_pickle)
 
-    print ("--> success\n\n" + creds_b64.decode('ascii') + "\n")
+    creds_string = lib.get_google_api_credentials_string(creds)
+
+    print ("--> success\n\n" + creds_string + "\n")
 
 if __name__ == '__main__':
     main()
-
--- validate_creds_base64.py
+++ validate_creds_base64.py
@@ -1,27 +1,9 @@
 from __future__ import print_function
-import os
 import sys
-import pickle
-import base64
-from googleapiclient.discovery import build
+import lib
 
 def main():
-    creds_b64 = os.getenv("GOOGLE_API_CREDENTIALS")
-    if not creds_b64:
-        print("ERROR: GOOGLE_API_CREDENTIALS env required", file=sys.stderr)
-        sys.exit(1)
-
-    try:
-        creds_pickle = base64.b64decode(creds_b64)
-    except Exception as err:
-        print("ERROR: invalid env value - base64 decode error(" + str(err) + ")", file=sys.stderr)
-        sys.exit(1)
-
-    try:
-        creds = pickle.loads(creds_pickle)
-    except Exception as err:
-        print("ERROR: invalid env value - pickle load error(" + str(err) + ")", file=sys.stderr)
-        sys.exit(1)
+    creds = lib.load_google_api_credentials_by_environment()
 
     if creds.expired:
         print("ERROR: creds token expired", file=sys.stderr)
